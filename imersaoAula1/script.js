7a29551a8507bdfdac0a5eb2993e4e4219cd1673
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2021-03-23 22:01:05 -0300

    firstcommit
    Aula 1 Imersao Dev - Conversor de Moedas

---
+++ imersaoAula1/script.js
@@ -0,0 +1,16 @@
+var valorEmDolarTexto = prompt("Qual o valor em dolar que voce quer converter?")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarNumero * 5.50
+
+var valorEmRealDecimal = valorEmReal.toFixed(2)
+
+alert(valorEmRealDecimal)
+
+
+//Revisao
+//variaveis var int - float - string
+//alert - parseInt - parseFloat - prompt
+//operacoes matematicas
+//comentarios finais
